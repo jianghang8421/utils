5b3e8f87274d87a34399367e2fb6801ba1411602
jianghang8421 <jianghang8421@163.com>
2018-12-04 16:22:25 +0800

    add scripts

---
+++ arm64/docker-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -x
+sudo apt-get update
+sudo apt-get install libltdl7
+wget https://download.docker.com/linux/ubuntu/dists/xenial/pool/stable/arm64/docker-ce_18.06.1~ce~3-0~ubuntu_arm64.deb
+sudo dpkg -i docker-ce_18.06.1~ce~3-0~ubuntu_arm64.deb
+sudo usermod -aG docker ubuntu
+newgrp docker
